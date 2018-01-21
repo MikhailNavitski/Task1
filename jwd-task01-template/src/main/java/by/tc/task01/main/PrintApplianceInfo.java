e522bc6859ba62033359e5c26374fa82069dabd9
liokree <lacosteren@gmail.com>
2018-01-21 22:50:38 +0300

    4

--- jwd-task01-template/src/main/java/by/tc/task01/main/PrintApplianceInfo.java
+++ jwd-task01-template/src/main/java/by/tc/task01/main/PrintApplianceInfo.java
@@ -5,28 +5,10 @@ import by.tc.task01.entity.*;
 class PrintApplianceInfo {
     static void print(Appliance appliance) {
 
-        if (appliance instanceof Oven) {
+        if (appliance != null) {
             System.out.println(appliance.toString());
-        } else if (appliance instanceof Laptop) {
-
-            System.out.println(appliance.toString());
-        } else if (appliance instanceof Refrigerator) {
-
-            System.out.println(appliance.toString());
-
-        } else if (appliance instanceof VacuumCleaner) {
-
-            System.out.println(appliance.toString());
-
-        } else if (appliance instanceof TabletPC) {
-
-            System.out.println(appliance.toString());
-
-        } else if (appliance instanceof Speakers) {
-
-            System.out.println(appliance.toString());
-        } else if (appliance == null) {
-            System.out.println("Нужный товар не найден\n");
+        } else {
+            System.out.println("Нужный товар не найден");
         }
     }
 }
