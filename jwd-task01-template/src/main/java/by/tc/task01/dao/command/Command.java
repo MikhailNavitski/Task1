b594e0f1d019fc2e10aea4f9d3fdcd508599f43e
liokree <lacosteren@gmail.com>
2018-01-22 15:52:13 +0300

    0

--- jwd-task01-template/src/main/java/by/tc/task01/dao/command/Command.java
+++ jwd-task01-template/src/main/java/by/tc/task01/dao/command/Command.java
@@ -2,8 +2,6 @@ package by.tc.task01.dao.command;
 
 import by.tc.task01.entity.Appliance;
 
-import java.io.IOException;
-
 public interface Command {
-    Appliance makeAppliance(String fileLine) throws IOException;
+    Appliance makeAppliance(String fileLine);
 }
