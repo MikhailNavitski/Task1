f2d8b0bf33e2fff8b9a1fdc770433bd0ac49d194
OSmol <Olga_Smolyakova@outlook.com>
2018-02-05 16:27:43 +0300

    Update Validator.java

--- jwd-task01-template/src/main/java/by/tc/task01/service/validation/Validator.java
+++ jwd-task01-template/src/main/java/by/tc/task01/service/validation/Validator.java
@@ -10,7 +10,8 @@ public class Validator {
 
     public static <E> boolean criteriaValidator(Criteria<E> criteria) {
         Map<E, Object> mapCriteria = criteria.getCriteria();
-        ValidationDirector director = new ValidationDirector();
+        ValidationDirector director = new ValidationDirector();// вот в дао попытался избавиться от кучи создаваемых объектов, а тут что сбойнуло?
+        // ты внуть класса своего глянь, что ты такое создаешь каждый раз
         int firstMachCount = 0;
         for (Map.Entry entry : mapCriteria.entrySet()) {
             String value = getValue(entry.getKey(), entry.getValue());
@@ -25,7 +26,7 @@ public class Validator {
         parameter = keyParameter + "=" + valueParameter + ",";
         String value = null;
         for (int i = 0; i < parameter.length(); i++) {
-            value = parameter.substring(parameter.indexOf("=") + 1, parameter.indexOf(","));
+            value = parameter.substring(parameter.indexOf("=") + 1, parameter.indexOf(","));// StringBuilder тебе в помощь
         }
         return value;
     }
